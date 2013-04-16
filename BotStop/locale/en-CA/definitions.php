3ceb19f27a8d3d65467ded6c9118edb257f306d8
Dan Fabulich <dan.fabulich@redfin.com>
2013-04-15 21:22:36 -0700

    fix bugs

--- BotStop/locale/en-CA/definitions.php
+++ BotStop/locale/en-CA/definitions.php
@@ -1,6 +1,6 @@
 <?php if (!defined('APPLICATION')) exit();
 $Definition['one'] = 'one';
-$Definition['two'] = 'one';
+$Definition['two'] = 'two';
 $Definition['three'] = 'three';
 $Definition['four'] = 'four';
 $Definition['five'] = 'five';
@@ -10,4 +10,4 @@ $Definition['eight'] = 'eight';
 $Definition['nine'] = 'nine';
 $Definition['Your humanity is suspect... Please try again.'] = "Your humanity is suspect... Please try again.";
 $Definition['How much is %1s %2s %3s?'] = 'How much is %1s %2s %3s?';
-$Definition['Humanity Check...  Not bots allowed!'] = "Humanity Check...  Not bots allowed!";
+$Definition['Humanity Check...  Not bots allowed!'] = "Humanity Check...  No bots allowed!";
